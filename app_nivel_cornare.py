f4c506b84d515e5217bb00e270f7557385232dba
jpablocaroo <jcaroosorio38@gmail.com>
2026-09-09 13:45:19 -0500

    Update default coordinates for API station

--- app_nivel_cornare.py
+++ app_nivel_cornare.py
@@ -20,8 +20,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Coordenadas por defecto (Institución Universitaria Pascual Bravo)
 # Se usan solo si la API no trae la latitud/longitud de la estación.
 # ------------------------------------------------------------------
-LAT_DEFECTO = 6.2766
-LON_DEFECTO = -75.5901
+LAT_DEFECTO = 6.1703
+LON_DEFECTO = -75.4359
 
 API_BASE_URL = "https://marco.cornare.gov.co/api/v1/estaciones"
 
